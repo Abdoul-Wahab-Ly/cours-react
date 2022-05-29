e830012e99bd22310128ff9327bbcc8e622c5c0e
Abdoul Wahab Ly <abdoulwahably64@gmail.com>
2022-05-29 17:49:51 +0000

    premier commit

---
+++ src/pages/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.
+      </p>
+      <br />
+
+      <p>
+        Qui iusto maiores aut sint maiores et minima corrupti. Sit esse
+        molestiae est exercitationem nemo ut alias autem quo tempora optio vel
+        officia dignissimos. Ut ratione voluptatem et accusamus quam qui sint
+        iure aut voluptas assumenda ut provident fugiat est voluptatem deleniti.
+        Est voluptas adipisci hic asperiores possimus quo cupiditate autem sed
+        quia error ad ducimus ipsum in porro velit est voluptatibus voluptatem!
+        Sed unde laborum est velit commodi aut quas optio aut quia pariatur qui
+        deserunt quasi id sunt cupiditate quo consequatur nobis? Sed laborum
+        vero ut adipisci galisum aut accusantium mollitia et similique quibusdam
+        aut quidem iste eos ipsum obcaecati.
+      </p>
+    </div>
+  );
+};
+
+export default About;
